85c30a37165a8ce360bf10f9bfc256fa69bb0312
win-chen <19295645+win-chen@users.noreply.github.com>
2024-11-12 17:00:40 -0500

    take scale instead of container in drag-helper

--- goldenrod/src/state/actions_internal.ts/pan-canvas.ts
+++ goldenrod/src/state/actions_internal.ts/pan-canvas.ts
@@ -1,41 +1,44 @@
-import { dragHelpers, type DragHelper } from "src/lib/svelte-utils/drag-helper";
-import { get, writable, type Writable } from "svelte/store";
-import type { RelativeRenderFrame } from "src/lib/svelte-utils/svelte-infinite-canvas/relative-render";
 import type { Container } from "pixi.js";
+import { dragHelpers, type DragHelper } from "src/lib/svelte-utils/drag-helper";
+import type { RelativeRenderFrame } from "src/lib/svelte-utils/svelte-infinite-canvas/relative-render";
+import { get, writable, type Writable } from "svelte/store";
 
-export const createPan = (pointerPos: Writable<{x: number, y: number}>, scalerContainer: Container, renderFrame: RelativeRenderFrame) => {
+export const createPan = (
+  pointerPos: Writable<{ x: number; y: number }>,
+  scalerContainer: Container,
+  renderFrame: RelativeRenderFrame
+) => {
   let helper: DragHelper | undefined;
   const start = () => {
     const { left, top } = get(renderFrame.originPoint);
     const originPos = writable({
       x: left,
-      y: top
+      y: top,
     });
 
     originPos.subscribe((val) => {
       renderFrame.originPoint.set({
         left: val.x,
-        top: val.y
+        top: val.y,
       });
     });
 
-    helper = dragHelpers(originPos, pointerPos, scalerContainer);
+    helper = dragHelpers(originPos, pointerPos, scalerContainer.scale.x);
 
     helper.start();
-  }
+  };
 
   const doPan = () => {
     helper?.update();
-  }
+  };
 
   const end = () => {
     helper?.end();
-  }
+  };
 
   return {
     start,
     doPan,
-    end
-  }
-
-}
+    end,
+  };
+};
