cd1597d8a48e5cc7efee79acce6fa7ec178e60ba
win-chen <19295645+win-chen@users.noreply.github.com>
2024-11-18 14:41:46 -0500

    wrap graphlib in a svelte store

---
+++ goldenrod/src/lib/svelte-utils/graphlib-store/writable-graph.test.ts
@@ -0,0 +1,52 @@
+import { describe, expect, it, vi } from "vitest";
+import { writableGraph } from "./writable-graph";
+
+describe("writableGraph", () => {
+  it("should add and remove nodes", () => {
+    const graphStore = writableGraph();
+
+    graphStore.setNode("A").update();
+    expect(graphStore.get().nodeCount()).toBe(1);
+
+    graphStore.removeNode("A").update();
+    expect(graphStore.get().nodeCount()).toBe(0);
+
+    graphStore.setNodes(["B", "C"]).update();
+    expect(graphStore.get().nodeCount()).toBe(2);
+  });
+
+  it("should add and remove edges - regular graph", () => {
+    const graphStore = writableGraph();
+
+    graphStore.setNode("A").setNode("B").setEdge("A", "B", "e-id").update();
+
+    expect(graphStore.get().edgeCount()).toBe(1);
+
+    graphStore.removeEdge("A", "B", "e-id").update();
+    expect(graphStore.get().edgeCount()).toBe(0);
+
+    graphStore
+      .setNode("C")
+      .setEdges([
+        { src: "A", target: "C" },
+        { src: "B", target: "C" },
+      ])
+      .update();
+    expect(graphStore.get().edgeCount()).toBe(2);
+  });
+
+  it("should update the store when graph changes", async () => {
+    const graphStore = writableGraph();
+
+    const spy = vi.fn();
+
+    graphStore.subscribe(spy);
+    spy.mockClear(); // subscribe calls the function initially
+
+    graphStore.setNode("A");
+    expect(spy).not.toHaveBeenCalled();
+    graphStore.setNode("B").update();
+
+    expect(spy).toHaveBeenCalled();
+  });
+});
---
+++ goldenrod/src/lib/svelte-utils/graphlib-store/writable-graph.ts
@@ -0,0 +1,69 @@
+import { Graph, type GraphOptions } from "@dagrejs/graphlib";
+import { get, writable } from "svelte/store";
+
+interface Edge {
+  src: string;
+  target: string;
+  edgeId?: string;
+}
+
+/**
+ * Creates a svelte store for a directed graph
+ * @returns a svelte store wrapping graphlib's Graph
+ */
+export const writableGraph = (
+  options: GraphOptions = {
+    multigraph: false,
+    directed: true,
+    compound: false,
+  }
+) => {
+  const graph = new Graph(options);
+  const store = writable(graph);
+
+  const methods = {
+    setNode: (id: string) => {
+      graph.setNode(id);
+      return result;
+    },
+    setNodes: (ids: string[]) => {
+      ids.forEach((id) => graph.setNode(id));
+      return result;
+    },
+    setEdge: (src: string, target: string, edgeId: string) => {
+      graph.setEdge(src, target, edgeId);
+      return result;
+    },
+    setEdges: (edges: Array<Edge>) => {
+      edges.forEach(({ src, target, edgeId }) => {
+        graph.setEdge(src, target, edgeId);
+      });
+      return result;
+    },
+    removeNode: (id: string) => {
+      graph.removeNode(id);
+      return result;
+    },
+    removeEdge: (src: string, target: string, edgeId?: string) => {
+      const edge = {
+        v: src,
+        w: target,
+        name: graph.isMultigraph() ? edgeId : undefined,
+      };
+
+      graph.removeEdge(edge);
+      return result;
+    },
+    get: () => get(store),
+    update: () => store.set(graph),
+  };
+
+  const result = {
+    ...store,
+    ...methods,
+  };
+
+  return result;
+};
+
+export type WritableGraph = ReturnType<typeof writableGraph>;
